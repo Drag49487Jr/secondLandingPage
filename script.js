74080dae6bd5acaacfdb805cf61fb4f85b41988d
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-11-05 23:03:17 -0600

    rid of js but add in anime.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-let grids = document.querySelectorAll('div');
-let num = 0;
-const delay = time => new Promise(resolve => setTimeout(resolve, time));
+// let grids = document.querySelectorAll('div');
+// let num = 0;
+// const delay = time => new Promise(resolve => setTimeout(resolve, time));
 
-grids.forEach(grid => {
-    delay(0).then(() => {
-        setTimeout(() => {
-            grid.style.backgroundColor = 'lightblue';
-        }, num += 100)
-    })
-    delay(500).then(() => {
-        setTimeout(() => {
-            grid.style.backgroundColor = 'rgb(228, 242, 255)';
-            grid.style.width = '90%';
-        }, num += 100)
-    })        
+// grids.forEach(grid => {
+//     delay(0).then(() => {
+//         setTimeout(() => {
+//             grid.style.backgroundColor = 'lightblue';
+//         }, num += 100)
+//     })
+//     delay(500).then(() => {
+//         setTimeout(() => {
+//             grid.style.backgroundColor = 'rgb(228, 242, 255)';
+//             grid.style.width = '90%';
+//         }, num += 100)
+//     })        
+// })
+
+let tl = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 750
+})
+tl.add({
+    targets: 'section div',
+    backgroundColor: 'rgb(197, 197, 255)',
+    
 })
